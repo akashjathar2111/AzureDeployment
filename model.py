b0c989d1bf31c41721c837930dee32c67117c25c
Akash Jathar <98139627+akashjathar2111@users.noreply.github.com>
2022-02-16 11:45:39 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,20 @@
+import pandas as pd 
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
+from sklearn.linear_model import LinearRegression
+
+
+
+df = pd.read_csv(r"C:\Users\Shri Ganesha\Documents\Excelr_data\Toyoto_Corrola.csv")
+data = df.iloc[:,2:]
+data = data.rename({'Age_08_04':'Age'},axis = 1)
+data = data.drop(df.index[[110,110,221,960,991]],axis = 0).reset_index()
+X = data[['Age','KM','HP','Cylinders','Gears','Weight']]
+Y = data['Price']
+reg = LinearRegression()
+reg.fit(X,Y)
+pickle.dump(reg,open('model.pkl','wb'))
+
+model = pickle.load(open('model.pkl','rb'))
